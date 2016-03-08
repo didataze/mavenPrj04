372e2e5fb463df3ec6d33d0b135e3d98b9fff7cb
didataze <didataze@gmail.com>
2016-03-08 12:35:30 +0100

    Ajout d angularjs 2eme  test

--- 04_presentation/src/main/webapp/index.jsp
+++ 04_presentation/src/main/webapp/index.jsp
@@ -1,34 +1,17 @@
-<html ng-app="myapp">
+<html>
 
 <head>
 
     <script src="lib/angular/angular.min.js"></script>
     <script src="lib/angular/angular-resource.min.js"></script>
+    <script src="lib/angular/angular-route.min.js"></script>
     <script src="js/controller.js"></script>
     <%--<script src="js/service.js"></script>--%>
 
 </head>
-<body ng-controller="productController">
+<body ng-app="myapp">
 
-<input type="text" ng-model="query" placeholder="Enter here">
-<hr>
-<select ng-model="order">
-    <option value="stockCode">Organiser par Code</option>
-    <option value="stockName">Organiser par Namse</option>
-</select>
-<hr>
-<table cellpadding="2" cellspacing="2" border="1">
-    <tr>
-        <th>code</th>
-        <th>id</th>
-        <th>name</th>
-    </tr>
-    <tr ng-repeat="pr in listProducts | filter:query | orderBy:order">
-        <td>{{pr.stockCode}}</td>
-        <td>{{pr.stockId}}</td>
-        <td>{{pr.stockName | uppercase}}</td>
-    </tr>
-</table>
+ <div ng-view></div>
 
 </body>
 </html>
--- 04_presentation/src/main/webapp/js/controller.js
+++ 04_presentation/src/main/webapp/js/controller.js
@@ -2,9 +2,76 @@
  * Created by stepinfo on 07/03/16.
  */
 
-var myapp = angular.module('myapp',[]);
+var app = angular.module('myapp',['ngRoute']);
 
-myapp.controller('productController',function($scope, $http){
+app.config(function($routeProvider){
+    $routeProvider
+        .when('/',{templateUrl: '../../partials/home.html',
+            controller: 'PostsCtrl'})
+        .when('/comments/:id',{templateUrl: '../../partials/comments.html',
+            controller: 'productController'})
+        .otherwise({redirectTo : '/'});
+});
+
+app.factory('PostFactory',function (){
+        var factory = {
+            posts : [
+                {
+                    "stock": [
+                        {
+                            "stockCode": "Nocha",
+                            "stockId": "1",
+                            "stockName": "GENM bcp blabla "
+                        },
+                        {
+                            "stockCode": "Idetica",
+                            "stockId": "4",
+                            "stockName": "quelques fonctions d"
+                        },
+                        {
+                            "stockCode": "Ali",
+                            "stockId": "5",
+                            "stockName": "web et du graphisme."
+                        },
+                        {
+                            "stockCode": "Ebra",
+                            "stockId": "6",
+                            "stockName": "Mais dans le cadre "
+                        }
+                    ]
+                }
+            ],
+            getPosts : function(){
+                return factory.posts;
+            },
+            getPost : function(id){
+                return factory.posts[0];
+            }
+
+        }
+    return factory;
+});
+
+
+//app.controller('PostsCtrl',function($scope, PostFactory){
+//
+//        $scope.listPosts = PostFactory.getPosts();
+//
+//});
+
+app.controller('PostsCtrl',function($scope, $http){
+    $http.get('http://localhost:8080/rest/emp/allStock').success(function(response){
+        $scope.listPosts = response.stock;
+    });
+});
+
+//app.controller('productController',function($scope, PostFactory){
+//
+//        $scope.listProducts = PostFactory.getPost().stockName;
+//
+//});
+
+app.controller('productController',function($scope, $http){
    $http.get('http://localhost:8080/rest/emp/allStock').success(function(response){
        $scope.listProducts = response.stock;
    });
